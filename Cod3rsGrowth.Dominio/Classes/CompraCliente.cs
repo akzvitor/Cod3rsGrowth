130177e0ed720ef51eb98edad7e1c6c65ba2884a
akzvitor <vitor_eterno@outlook.com>
2024-05-15 09:35:27 -0300

    remove prop Endereco e muda a prop Genero para Lista de Enum Genero

--- Cod3rsGrowth.Dominio/Classes/CompraCliente.cs
+++ Cod3rsGrowth.Dominio/Classes/CompraCliente.cs
@@ -7,7 +7,6 @@
         public string Nome { get; set; }
         public string Telefone { get; set; }
         public string Email { get; set; }
-        public string Endereco { get; set; }
         public List<Obra> Produtos { get; set; }
         public decimal ValorCompra { get; set; }
         public DateTime DataCompra { get; set; }
