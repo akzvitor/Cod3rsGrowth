e8610274cf89f95080ce285e9beae6a68388592c
akzvitor <vitor_eterno@outlook.com>
2024-05-16 10:46:25 -0300

    configura o projeto de teste base

---
+++ Cod3rsGrowth.Testes/ConfiguracaoAmbienteTeste/TesteBase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cod3rsGrowth.Testes.ConfiguracaoAmbienteTeste
+{
+
+    public abstract class TesteBase : IDisposable
+    {
+
+        protected ServiceProvider ServiceProvider;
+
+        protected TesteBase()
+        {
+            ServiceProvider = ObterServiceCollection().BuildServiceProvider();
+        }
+
+        private IServiceCollection ObterServiceCollection()
+        {
+            var servicos = new ServiceCollection();
+            ModuloDeInjecao.BindService(servicos);
+            return servicos;
+        }
+
+        public virtual void Dispose()
+        {
+            ServiceProvider.Dispose();
+        }
+    }
+}
--- Cod3rsGrowth.Testes/ModuloDeInjecao.cs
+++ Cod3rsGrowth.Testes/ModuloDeInjecao.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace Cod3rsGrowth.Testes
 {
     public class ModuloDeInjecao
     {
+
+        public static void BindService(ServiceCollection servicos)
+        {
+        }
     }
 }
