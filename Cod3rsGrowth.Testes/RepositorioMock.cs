9402edcaff54220a8142b241be059e0ebc4db4b1
akzvitor <vitor_eterno@outlook.com>
2024-05-16 13:50:07 -0300

    cria interface de repositorio base e o repositorio mock

---
+++ Cod3rsGrowth.Testes/RepositorioMock.cs
@@ -0,0 +1,19 @@
+﻿using Cod3rsGrowth.Dominio.Classes;
+using Cod3rsGrowth.Dominio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cod3rsGrowth.Testes
+{
+    public class RepositorioMock : IRepositorio
+    {
+        
+        public List<Obra> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
