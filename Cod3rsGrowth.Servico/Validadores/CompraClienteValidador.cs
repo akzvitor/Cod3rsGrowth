329b4f4ec996d24b1a5bb363899c034413c193e0
akzvitor <vitor_eterno@outlook.com>
2024-06-03 11:19:26 -0300

    refatora a funcao que calcula cpf

--- Cod3rsGrowth.Servico/Validadores/CompraClienteValidador.cs
+++ Cod3rsGrowth.Servico/Validadores/CompraClienteValidador.cs
@@ -49,14 +49,6 @@ namespace Cod3rsGrowth.Servico.Validadores
 
         public static bool EhCpfValido(string cpf)
         {
-            cpf = new string(cpf.Where(char.IsDigit).ToArray());
-
-            if (cpf.Length != 11)
-                return false;
-
-            if (cpf.Distinct().Count() == 1)
-                return false;
-
             int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
             int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
             string tempCpf;
@@ -64,38 +56,64 @@ namespace Cod3rsGrowth.Servico.Validadores
             int soma;
             int resto;
 
+            cpf = new string(cpf.Where(char.IsDigit).ToArray());
+
+            if (cpf.Length != 11)
+            {
+                return false;
+            }
+
+            if (cpf.Distinct().Count() == 1)
+            { 
+                return false; 
+            }
+
             cpf = cpf.Trim();
             cpf = cpf.Replace(".", "").Replace("-", "");
 
             if (cpf.Length != 11)
+            {
                 return false;
+            }
 
             tempCpf = cpf.Substring(0, 9);
             soma = 0;
 
             for (int i = 0; i < 9; i++)
+            {
                 soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+            }
 
             resto = soma % 11;
 
             if (resto < 2)
+            {
                 resto = 0;
+            }
             else
+            {
                 resto = 11 - resto;
+            }
 
             digito = resto.ToString();
             tempCpf += digito;
             soma = 0;
 
             for (int i = 0; i < 10; i++)
+            {
                 soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+            }
 
             resto = soma % 11;
 
             if (resto < 2)
+            {
                 resto = 0;
+            }
             else
+            {
                 resto = 11 - resto;
+            }
 
             digito += resto.ToString();
 
