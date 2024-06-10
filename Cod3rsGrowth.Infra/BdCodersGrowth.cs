75d5e7bffc5ad98c828c1c82ef8ab6098827e1e6
akzvitor <vitor_eterno@outlook.com>
2024-06-10 12:35:54 -0300

    cria classe de conexao de dados

---
+++ Cod3rsGrowth.Infra/BdCodersGrowth.cs
@@ -0,0 +1,14 @@
+﻿using Cod3rsGrowth.Dominio.Classes;
+using LinqToDB;
+using LinqToDB.Data;
+
+namespace Cod3rsGrowth.Infra
+{
+    public class BdCodersGrowth : DataConnection 
+    {
+        public BdCodersGrowth() : base("StringConexao") { }
+
+        public ITable<CompraCliente> CompraCliente => this.GetTable<CompraCliente>();
+        public ITable<Obra>          Obra          => this.GetTable<Obra>();
+    }
+}
