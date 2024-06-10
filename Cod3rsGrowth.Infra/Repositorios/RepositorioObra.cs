ee7f5c4e798dfe61aa2af624ecdc7424866728ae
akzvitor <vitor_eterno@outlook.com>
2024-06-10 10:37:03 -0300

    cria repositorios concretos de obra e compra

---
+++ Cod3rsGrowth.Infra/Repositorios/RepositorioObra.cs
@@ -0,0 +1,32 @@
+﻿using Cod3rsGrowth.Dominio.Classes;
+using Cod3rsGrowth.Dominio.Interfaces;
+
+namespace Cod3rsGrowth.Infra.Repositorios
+{
+    public class RepositorioObra : IRepositorio<Obra>
+    {
+        public List<Obra> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+        public Obra ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Obra Criar(Obra obra)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Obra Editar(Obra obra)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remover(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
