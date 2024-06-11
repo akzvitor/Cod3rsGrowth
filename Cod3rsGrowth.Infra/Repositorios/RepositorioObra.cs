fcfeaa3bf276c3c25842dc494c95d8ba59c7c4eb
akzvitor <vitor_eterno@outlook.com>
2024-06-11 08:54:09 -0300

    cria filtro de obras para aplicar na funcao obter todos

--- Cod3rsGrowth.Infra/Repositorios/RepositorioObra.cs
+++ Cod3rsGrowth.Infra/Repositorios/RepositorioObra.cs
@@ -1,4 +1,5 @@
 ﻿using Cod3rsGrowth.Dominio.Classes;
+using Cod3rsGrowth.Dominio.Enums;
 using Cod3rsGrowth.Dominio.Interfaces;
 using Cod3rsGrowth.Infra.ConexaoDeDados;
 
@@ -6,11 +7,13 @@ namespace Cod3rsGrowth.Infra.Repositorios
 {
     public class RepositorioObra : IRepositorio<Obra>
     {
-        private DbCodersGrowth _db;
+        private readonly DbCodersGrowth _db;
+
         public RepositorioObra(DbCodersGrowth conexaoComBancoDeDados) 
         {
             _db = conexaoComBancoDeDados;
         }
+
         public List<Obra> ObterTodos()
         {
             var query = from o in _db.Obra
@@ -38,6 +41,35 @@ namespace Cod3rsGrowth.Infra.Repositorios
         public void Remover(int id)
         {
             throw new NotImplementedException();
+        }
+
+        public static IQueryable<Obra> Filtro(IQueryable<Obra> obras, string? autor = null, string? titulo = null, List<Genero> listaDeGeneros = null, Formato? formato = null, bool? foiFinalizada = null) 
+        {
+            if (!string.IsNullOrEmpty(autor))
+            {
+                obras = obras.Where(o => o.Autor == autor);
+            }
+
+            if (!string.IsNullOrEmpty(titulo))
+            {
+                obras = obras.Where(o => o.Titulo.Contains(titulo));
+            }
+
+            if (formato.HasValue)
+            {
+                obras = obras.Where(o => o.Formato == formato.Value);
+            }
+
+            if (foiFinalizada.HasValue)
+            {
+                obras = obras.Where(o => o.FoiFinalizada ==  foiFinalizada.Value);
+            }
+            
+            if (listaDeGeneros != null && listaDeGeneros.Any())
+            {
+                obras = obras.Where(o => o.Generos.Any(g => listaDeGeneros.Contains(g)));
+            }
+            return obras;
         }
     }
 }
