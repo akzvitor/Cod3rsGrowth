092f7cb99eef8fb07e645ff1dfdb56ae605f40b3
akzvitor <vitor_eterno@outlook.com>
2024-06-11 09:23:16 -0300

    implementa obter todas as compras com filtro

--- Cod3rsGrowth.Infra/Repositorios/RepositorioCompraCliente.cs
+++ Cod3rsGrowth.Infra/Repositorios/RepositorioCompraCliente.cs
@@ -1,13 +1,24 @@
 ﻿using Cod3rsGrowth.Dominio.Classes;
 using Cod3rsGrowth.Dominio.Interfaces;
+using Cod3rsGrowth.Infra.ConexaoDeDados;
 
 namespace Cod3rsGrowth.Infra.Repositorios
 {
     public class RepositorioCompraCliente : IRepositorio<CompraCliente>
     {
+        private readonly DbCodersGrowth _db;
+
+        public RepositorioCompraCliente(DbCodersGrowth conexaoComBancoDeDados) 
+        {
+            _db = conexaoComBancoDeDados;
+        }
+
         public List<CompraCliente> ObterTodos()
         {
-            throw new NotImplementedException();
+            var query = Filtro(_db.ComprasCliente, nome: "Vitor");
+            var comprasFiltradas = query.ToList();
+
+            return comprasFiltradas;
         }
 
         public CompraCliente ObterPorId(int id)
@@ -28,6 +39,27 @@ namespace Cod3rsGrowth.Infra.Repositorios
         public void Remover(int id)
         {
             throw new NotImplementedException();
+        }
+
+        public static IQueryable<CompraCliente> Filtro(IQueryable<CompraCliente> compras, string? nome = null, 
+                                                        DateTime? dataDaCompra = null, decimal? valorDaCompra = null)
+        {
+            if (!string.IsNullOrEmpty(nome))
+            {
+                compras = compras.Where(c => c.Nome.Contains(nome));
+            }
+
+            if (dataDaCompra.HasValue)
+            {
+                compras = compras.Where(c => c.DataCompra == dataDaCompra.Value);
+            }
+
+            if(valorDaCompra.HasValue)
+            {
+                compras = compras.Where(c => c.ValorCompra == valorDaCompra.Value);
+            }
+
+            return compras;
         }
     }
 }
