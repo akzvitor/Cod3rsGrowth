e88cf1bafc6533c302822dff2e87c4b8c03080b3
akzvitor <vitor_eterno@outlook.com>
2024-06-13 11:19:32 -0300

    cria classe de migracao da tabela de compras

--- Cod3rsGrowth.Infra/Migracoes/AdicionaTabelaComprasCliente.cs
+++ Cod3rsGrowth.Infra/Migracoes/AdicionaTabelaComprasCliente.cs
@@ -5,6 +5,22 @@ namespace Cod3rsGrowth.Infra.Migracoes
     [Migration(20241306110300)]
     public class AdicionaTabelaComprasCliente : Migration
     {
+        public override void Up()
+        {
+            Create.Table("Compras Cliente")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("CPF").AsString()
+                .WithColumn("Nome").AsString()
+                .WithColumn("Telefone").AsString()
+                .WithColumn("E-mail").AsString()
+                //.WithColumn("Produtos")
+                .WithColumn("Valor").AsDecimal()
+                .WithColumn("Data da compra").AsDateTime();
+        }
 
+        public override void Down()
+        {
+            Delete.Table("Compras Cliente");
+        }
     }
 }
