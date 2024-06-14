6f5382137ae894cfef0ec3f96161fea5923be8ba
akzvitor <vitor_eterno@outlook.com>
2024-06-14 11:02:59 -0300

    configura o runner e chama os servicos na classe program do forms

--- Cod3rsGrowth.Forms/Program.cs
+++ Cod3rsGrowth.Forms/Program.cs
@@ -1,3 +1,8 @@
+using Cod3rsGrowth.Infra.Migracoes;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+
 namespace Cod3rsGrowth.Forms
 {
     internal static class Program
@@ -6,12 +11,39 @@ namespace Cod3rsGrowth.Forms
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+
+            using (var serviceProvider = CriarServicos())
+            using (var escopo = serviceProvider.CreateScope()) 
+            {
+                AtualizarBancoDeDados(escopo.ServiceProvider);
+            }
+        }
+
+        private static ServiceProvider CriarServicos()
+        {
+            var stringDeConexao = ConfigurationManager.ConnectionStrings["StringConexao"].ToString();
+
+            return new ServiceCollection()
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    .AddSQLite()
+                    .WithGlobalConnectionString(stringDeConexao)
+                    .ScanIn(typeof(AdicionaTabelaObras).Assembly).For.Migrations())
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                .BuildServiceProvider(false);
+        }
+
+        private static void AtualizarBancoDeDados(IServiceProvider serviceProvider)
+        {
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            runner.MigrateUp();
         }
     }
 }
