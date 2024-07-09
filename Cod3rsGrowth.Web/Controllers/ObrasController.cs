4ee44fcdd150425640d728c09263be7201175ea8
akzvitor <vitor_eterno@outlook.com>
2024-07-09 14:04:37 -0300

    feat: inicia configuracao da api web

--- Cod3rsGrowth.Web/Controllers/ObrasController.cs
+++ Cod3rsGrowth.Web/Controllers/ObrasController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Cod3rsGrowth.Dominio.Entidades;
+using Cod3rsGrowth.Servico.Servicos;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cod3rsGrowth.Web.Controllers
@@ -7,5 +9,23 @@ namespace Cod3rsGrowth.Web.Controllers
     [ApiController]
     public class ObrasController : ControllerBase
     {
+        private readonly ServicoObra _servicoObra;
+        public ObrasController(ServicoObra servicoObra)
+        {
+            _servicoObra = servicoObra;
+        }
+
+        [HttpPost]
+        public IActionResult Criar([FromBody] Obra obra)
+        {
+            if (obra == null)
+            {
+                return BadRequest();
+            }
+
+            var novaObra = _servicoObra.Criar(obra);
+
+            return Created($"obra/{novaObra.Id}", novaObra);
+        }
     }
 }
---
+++ Cod3rsGrowth.Web/ModuloDeInjecao/ModuloDeInjecaoWeb.cs
@@ -0,0 +1,24 @@
+﻿using Cod3rsGrowth.Dominio.Interfaces;
+using Cod3rsGrowth.Infra.Repositorios;
+using Cod3rsGrowth.Servico.Servicos;
+using Cod3rsGrowth.Servico.Validadores;
+
+namespace Cod3rsGrowth.Web.ModuloDeInjecao
+{
+    public static class ModuloDeInjecaoWeb
+    {
+        public static void BindService(ServiceCollection servicos)
+        {
+            servicos.AddScoped<ServicoObra>();
+            servicos.AddScoped<ServicoCompraCliente>();
+
+            servicos.AddScoped<IRepositorioObra, RepositorioObra>();
+            servicos.AddScoped<IRepositorioCompraCliente, RepositorioCompraCliente>();
+
+            servicos.AddScoped<ObraValidador>();
+            servicos.AddScoped<CompraClienteValidador>();
+
+            servicos.AddScoped(provider => new DbCodersGrowth(stringDeConexao));
+        }
+    }
+}
