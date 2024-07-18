7efcf4ac05ffee6d7c8b9d4b268d7f7290137736
akzvitor <vitor_eterno@outlook.com>
2024-07-18 14:02:58 -0300

    salva alteracoes

--- Cod3rsGrowth.Web/wwwroot/webapp/Component.js
+++ Cod3rsGrowth.Web/wwwroot/webapp/Component.js
@@ -1,12 +1,32 @@
 ﻿sap.ui.define([
-    "sap/ui/core/UIComponent"
-], (UIComponent) => {
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/resource/ResourceModel"
+], (UIComponent, JSONModel, ResourceModel) => {
     "use strict";
 
-    return UIComponent.extend("", {
+    return UIComponent.extend("ui5.codersgrowth.Component", {
+        metadata: {
+            "interfaces": ["sap.ui.core.IAsyncContentCreation"],
+            "rootView": {
+                "viewName": "ui5.codersgrowth.view.App",
+                "type": "XML",
+                "id": "app"
+            }
+        },
+
         init() {
-            // call the init function of the parent
             UIComponent.prototype.init.apply(this, arguments);
+
+            const oData = {
+                recipient: {
+                    name: "World"
+                }
+            };
+
+            const i18nModel = new ResourceModel({
+                bundleName: "ui5.codersgrowth.i18n.i18n"
+            });
+            this.setModel(i18nModel, "i18n");
         }
     });
 });
