3b45bd71eeacf2d63cedbd14c2eb77b30157c240
akzvitor <vitor_eterno@outlook.com>
2024-07-25 11:25:07 -0300

    formata dados da tabela e adiciona inputs de filtro

--- Cod3rsGrowth.Web/wwwroot/webapp/model/formatter.js
+++ Cod3rsGrowth.Web/wwwroot/webapp/model/formatter.js
@@ -19,6 +19,17 @@ sap.ui.define([
             });
 
             return oDateTimeFormat.format(new Date(data));
+        },
+
+        formatarCpf(cpf) {
+            return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+        },
+
+        formatarTelefone(telefone) {
+            telefone = telefone.replace(/\D/g, '');
+            telefone = telefone.replace(/(\d{2})(\d)/, "($1) $2");
+            telefone = telefone.replace(/(\d)(\d{4})$/, "$1-$2");
+            return telefone;
         }
     };
 });
