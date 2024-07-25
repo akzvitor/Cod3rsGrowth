d50f074f8c7ea48bf0d6c6665b1fb2e82a9107de
akzvitor <vitor_eterno@outlook.com>
2024-07-25 14:00:44 -0300

    comeca a implementar filtros

--- Cod3rsGrowth.Web/wwwroot/webapp/controller/Listagem.controller.js
+++ Cod3rsGrowth.Web/wwwroot/webapp/controller/Listagem.controller.js
@@ -6,16 +6,38 @@ sap.ui.define([
     "use strict";
 
     const API_URL = "http://localhost:5070/api/Compras?";
-    const NOMDE_DO_MODELO = "restCompras";
+    const NOME_DO_MODELO = "restCompras";
+    const ID_NOME_FILTRO_INPUT = "nomeFiltroInput";
+    const ID_CPF_FILTRO_INPUT = "cpfFiltroInput";
+    const ID_DATAINICIAL_FILTRO_INPUT= "dataInicialFiltroInput";
+    const ID_DATAFINAL_FILTRO_INPUT= "dataFinalFiltroInput";
 
     return BaseController.extend("ui5.coders.controller.Listagem", {
         formatter: formatter,
-        
-        onInit() {
+
+        InicializarDados : function() {
             fetch(API_URL)
                 .then((res) => res.json())
-                .then((data) => this.getView().setModel(new JSONModel(data), NOMDE_DO_MODELO))
+                .then((data) => this.getView().setModel(new JSONModel(data), NOME_DO_MODELO))
                 .catch((err) => console.error(err));
+        },
+
+        onInit() {
+            this.InicializarDados();
+        },
+
+        AoAlterarInputFiltro() {
+            const inputNome = this.oView.byId(ID_NOME_FILTRO_INPUT).getValue();
+            const inputCpf = this.oView.byId(ID_CPF_FILTRO_INPUT).getValue();
+            const inputDataInicial = this.oView.byId(ID_DATAINICIAL_FILTRO_INPUT).getValue();
+            const inputDataFinal = this.oView.byId(ID_DATAFINAL_FILTRO_INPUT).getValue();
+            
+            if (inputNome) {API_URL += "nomeCliente=" + inputNome + "&"}
+            if (inputCpf) {API_URL += "cpf=" + inputCpf + "&"}
+            if (inputDataInicial) {API_URL += "dataInicial=" + inputDataInicial + "&"}
+            if (inputDataFinal) {API_URL += "dataFinal=" + inputDataFinal + "&"}
+
+            this.InicializarDados();
         }
     });
 });
--- Cod3rsGrowth.Web/wwwroot/webapp/model/formatter.js
+++ Cod3rsGrowth.Web/wwwroot/webapp/model/formatter.js
@@ -30,6 +30,6 @@ sap.ui.define([
             telefone = telefone.replace(/(\d{2})(\d)/, "($1) $2");
             telefone = telefone.replace(/(\d)(\d{4})$/, "$1-$2");
             return telefone;
-        }
+        },
     };
 });
