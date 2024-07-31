a3545f855532c3a8162485be78df6834319d5a96
akzvitor <vitor_eterno@outlook.com>
2024-07-31 13:48:31 -0300

    exibe lista de produtos na pagina de criar compra

--- Cod3rsGrowth.Web/wwwroot/webapp/controller/CriacaoCompra.controller.js
+++ Cod3rsGrowth.Web/wwwroot/webapp/controller/CriacaoCompra.controller.js
@@ -1,9 +1,22 @@
 sap.ui.define([
-    "ui5/coders/controller/BaseController"
-], (BaseController) => {
+    "ui5/coders/controller/BaseController",
+    "sap/ui/model/json/JSONModel"
+], (BaseController, JSONModel) => {
     "use strict";
 
+    const API_URL = "http://localhost:5070/api/Obras";
+    const NOME_DO_MODELO= "restObras";
+
     return BaseController.extend("ui5.coders.controller.CriacaoVenda", {
-        onInit() {}
+        onInit() {
+            this._inicializarDados(API_URL);
+        },
+
+        _inicializarDados(urlDaApi) {
+            fetch(urlDaApi)
+                .then((res) => res.json())
+                .then((data) => this.getView().setModel(new JSONModel(data), NOME_DO_MODELO))
+                .catch((err) => console.error(err));
+        }
     });
 });
