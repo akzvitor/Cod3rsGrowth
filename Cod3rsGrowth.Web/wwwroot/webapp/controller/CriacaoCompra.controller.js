33c12c2abaa5306b8294611cc551a70e42c84f1a
akzvitor <vitor_eterno@outlook.com>
2024-07-31 08:41:37 -0300

    cria base da view e controller para a tela de criacao de compras

---
+++ Cod3rsGrowth.Web/wwwroot/webapp/controller/CriacaoCompra.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+    "ui5/coders/controller/BaseController"
+], (BaseController) => {
+    "use strict";
+
+    return BaseController.extend("ui5.coders.controller.CriacaoVenda", {
+
+    });
+});
