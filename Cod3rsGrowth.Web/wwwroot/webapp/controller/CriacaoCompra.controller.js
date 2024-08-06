b7672fefa42ae4d3b22ac56c7607694a83b34b2e
akzvitor <vitor_eterno@outlook.com>
2024-08-06 13:54:10 -0300

    torna selecao de obras obrigatorio

--- Cod3rsGrowth.Web/wwwroot/webapp/controller/CriacaoCompra.controller.js
+++ Cod3rsGrowth.Web/wwwroot/webapp/controller/CriacaoCompra.controller.js
@@ -15,6 +15,7 @@ sap.ui.define([
     const ID_EMAIL_FORM_INPUT = "emailFormInput";
     const ID_CPF_FORM_INPUT = "cpfFormInput";
     const ID_TELEFONE_FORM_INPUT = "telefoneFormInput";
+    const ID_ERRO_VALIDACAO_PRODUTOS = "mensagemErroProdutos";
 
     return BaseController.extend("ui5.coders.controller.CriacaoCompra", {
         formatter: formatter,
@@ -60,7 +61,11 @@ sap.ui.define([
 
             const dadosSaoValidos = validator.validarDados(inputNome, inputEmail, inputTelefone, inputCpf);
 
+            if(oObrasSelecionadas.listaIdsSelecionados.length === erroListaDeProdutosVazia)
+                this.oView.byId(ID_ERRO_VALIDACAO_PRODUTOS).setVisible(true);
+
             if (dadosSaoValidos && oObrasSelecionadas.listaIdsSelecionados.length !== erroListaDeProdutosVazia)
+                this.oView.byId(ID_ERRO_VALIDACAO_PRODUTOS).setVisible(false);
                 this._postData(data);
         },
 
