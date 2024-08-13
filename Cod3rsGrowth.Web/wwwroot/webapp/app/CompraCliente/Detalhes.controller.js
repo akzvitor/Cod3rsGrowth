ecc1e250fc203cfd768afe49379a33137274fe33
akzvitor <vitor_eterno@outlook.com>
2024-08-13 11:48:00 -0300

    cria base para view e controller da pagina de detalhes

---
+++ Cod3rsGrowth.Web/wwwroot/webapp/app/CompraCliente/Detalhes.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+    "ui5/coders/app/common/BaseController"
+], (BaseController) => {
+    "use strict";
+
+    return BaseController.extend("ui5.coders.app.CompraCliente.Detalhes", {
+        onInit() {}
+    });
+});
