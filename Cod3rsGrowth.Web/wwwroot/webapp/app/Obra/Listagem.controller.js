ff6a1e2da16af5085f0a775b586371275b213d75
akzvitor <vitor_eterno@outlook.com>
2024-08-26 12:49:21 -0300

    implementa filtro por status - bool

--- Cod3rsGrowth.Web/wwwroot/webapp/app/Obra/Listagem.controller.js
+++ Cod3rsGrowth.Web/wwwroot/webapp/app/Obra/Listagem.controller.js
@@ -9,6 +9,7 @@ sap.ui.define([
     const MODELO_OBRAS = "restObras";
     const ID_INPUT_TITULO = "tituloFiltroInput";
     const ID_INPUT_AUTOR = "autorFiltroInput";
+    const ID_COMBOBOX_STATUS = "statusFiltroComboBox"
 
     return BaseController.extend("ui5.coders.app.Obra.Listagem", {
         formatter: formatter,
@@ -17,15 +18,19 @@ sap.ui.define([
             this.aoCoincidirRota(ROTA_LISTAGEM_OBRAS, API_OBRAS_URL, MODELO_OBRAS);
         },
 
-        aoAlterarInputFiltro(oEvent) {
+        aoAlterarInputFiltro() {
             this.processarAcao(() => {
                 let urlFiltro = "http://localhost:5070/api/Obras?";
                 const inputTitulo = this.oView.byId(ID_INPUT_TITULO).getValue();
                 const inputAutor = this.oView.byId(ID_INPUT_AUTOR).getValue();
+                const comboBoxStatus = this.oView.byId(ID_COMBOBOX_STATUS).getSelectedKey();
+                console.log(comboBoxStatus);
 
                 if (inputTitulo) { urlFiltro += "Titulo=" + inputTitulo + "&"; }
 
                 if (inputAutor) { urlFiltro += "Autor=" + inputAutor + "&"; }
+
+                if (comboBoxStatus) { urlFiltro += "Finalizada=" + comboBoxStatus + "&"; }
 
                 this.inicializarDados(urlFiltro, MODELO_OBRAS);
             });
