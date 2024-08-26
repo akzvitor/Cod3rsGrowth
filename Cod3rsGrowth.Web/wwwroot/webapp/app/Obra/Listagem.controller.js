ab03cb9b88585a7ef7d0774504bdfdf85a8821c0
akzvitor <vitor_eterno@outlook.com>
2024-08-26 08:57:44 -0300

    cria base da view e controller da pagina de obras

---
+++ Cod3rsGrowth.Web/wwwroot/webapp/app/Obra/Listagem.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "ui5/coders/app/common/BaseController",
+    'ui5/coders/model/formatter'
+], (BaseController, formatter) => {
+    "use strict";
+    return BaseController.extend("ui5.coders.app.Obra.Listagem", {
+        formatter: formatter,
+
+        
+    });
+});
